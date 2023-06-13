d2441d1cde8a9fcd494ce407c78e981b3f969ada
pegah k <pegika327@gmail.com>
2023-06-13 20:46:06 +0330

    first commit

---
+++ src/App.vue
@@ -0,0 +1,153 @@
+<template>
+  <div id="app">
+    <div class="header">Profiles List</div>
+    <div class="content">
+      <div class="buttons">
+        <button @click="sortAsc">▲</button>
+        <button @click="sortDesc">▼</button>
+      </div>
+
+      <ProfileCard
+        v-for="(profile, index) in profiles"
+        :key="index"
+        :profile="profile"
+        class="profile"
+      />
+
+      <div class="icons-note">
+        Icons made by
+        <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from
+        <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ProfileCard from "./components/ProfileCard";
+
+export default {
+  name: "App",
+
+  components: {
+    ProfileCard
+  },
+
+  data() {
+    return {
+      profiles: [
+        {
+          id: 1,
+          name: "Wojciech",
+          email: "wojciech@poz.pl",
+          description: "Anaesthesiologist",
+          likes: 34
+        },
+        {
+          id: 2,
+          name: "Maria",
+          email: "maria@poz.pl",
+          description: "Radiologist",
+          likes: 28
+        },
+        {
+          id: 3,
+          name: "Anna",
+          email: "anna@poz.pl",
+          description: "Surgeon",
+          likes: 53
+        }
+      ]
+    };
+  },
+
+  methods: {
+    sortAsc() {
+      this.profiles.sort(function(a, b) {
+        return a.likes - b.likes;
+      });
+    },
+
+    sortDesc() {
+      this.profiles.sort(function(a, b) {
+        return b.likes - a.likes;
+      });
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Roboto", helvetica, arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  padding: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  position: relative;
+
+  background: linear-gradient(
+    135deg,
+    rgba(65, 184, 131, 0.9),
+    rgba(52, 73, 94, 0.9)
+  );
+
+  font-size: 1.5em;
+}
+
+button {
+  display: block;
+  padding: 1em;
+  width: 100%;
+  background-color: #41B883;
+  border: 1px solid;
+  color: #fff;
+  cursor: pointer;
+  font-size: 0.75em;
+  font-weight: 600;
+  text-shadow: 0 1px 0 rgba(black, 0.2);
+}
+
+.content {
+  width: 100%;
+  min-width: 300px;
+  padding: 2em;
+  margin-top: 30px;
+  position: relative;
+  background: rgba(0, 0, 0, 0.15);
+}
+
+@media screen and (min-width: 600px) {
+  .content {
+    width: 50vw;
+    max-width: 15em;
+  }
+}
+
+.content::before {
+  content: "";
+  position: absolute;
+  top: -2px;
+  left: 0;
+  height: 2px;
+  width: 100%;
+  background: #35c3c1;
+}
+
+.buttons {
+  display: flex;
+  box-shadow: 0 10px 10px rgba(0, 0, 0, 0.25), 0 5px 5px rgba(0, 0, 0, 0.22);
+}
+
+.profile {
+  margin-top: 20px;
+}
+
+.icons-note {
+  margin-top: 30px;
+  font-size: 10px;
+}
+</style>
---
+++ src/components/ProfileCard.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="profile">
+    <div class="card">
+      <DoctorIcon class="avatar"/>
+      <div class="data">
+        <div class="profile-content">
+          <div>
+            <strong>{{profile.name}}</strong>
+            <a :href="profile.email" class="email">{{profile.email}}</a>
+          </div>
+          <div class="description">{{profile.description}}</div>
+        </div>
+        <div class="likes">
+          <span class="likes-icon">💚</span>
+          <span class="likes-value">{{profile.likes}}</span>
+        </div>
+      </div>
+    </div>
+    <div class="comment">
+      <input class="comment-input" placeholder="Write your comment...">
+    </div>
+  </div>
+</template>
+
+<script>
+import DoctorIcon from "./DoctorIcon";
+
+export default {
+  name: "ProfileCard",
+
+  components: {
+    DoctorIcon
+  },
+
+  props: {
+    profile: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
+
+<style>
+.card {
+  display: flex;
+  align-items: center;
+  border: solid 1px rgb(172, 172, 172);
+  border-radius: 3px;
+  padding: 10px;
+  font-size: 14px;
+  color: rgb(82, 82, 82);
+  background-color: #fff;
+  box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+}
+
+.avatar {
+  width: 64px;
+  min-width: 64px;
+  height: 64px;
+  border: 1px solid rgb(172, 172, 172);
+  border-radius: 100%;
+  box-shadow: 0 10px 10px rgba(0, 0, 0, 0.25), 0 5px 5px rgba(0, 0, 0, 0.22);
+}
+
+.data {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+
+.profile-content {
+  display: flex;
+  flex-direction: column;
+  text-align: left;
+  margin-left: 15px;
+}
+
+.email {
+  margin-left: 15px;
+}
+
+.description {
+  margin-top: 5px;
+}
+
+.likes {
+  color: rgb(76, 202, 114);
+  margin-top: 10px;
+  margin-left: 15px;
+}
+
+.likes-value {
+  margin-left: 5px;
+}
+
+.comment {
+  display: flex;
+  width: 100%;
+}
+
+.comment-input {
+  width: 100%;
+}
+</style>
