ef46ad1d246e4bf2401a57b30756d41e93ae7515
pegah k <pegika327@gmail.com>
2023-06-13 22:54:17 +0330

    Add search feature filter the list by profile name property.

--- src/App.vue
+++ src/App.vue
@@ -1,153 +1,168 @@
 <template>
-  <div id="app">
-    <div class="header">Profiles List</div>
-    <div class="content">
-      <div class="buttons">
-        <button @click="sortAsc">▲</button>
-        <button @click="sortDesc">▼</button>
-      </div>
+    <div id="app">
+        <div class="header">Profiles List</div>
+        <div class="content">
+            <input  v-model="searchedName" class="search" placeholder="Search by DR Name">
+            <div class="buttons">
+                <button @click="sortAsc">▲</button>
+                <button @click="sortDesc">▼</button>
+            </div>
 
-      <ProfileCard
-        v-for="profile in profiles"
-        :key="profile.id"
-        :profile="profile"
-        class="profile"
-      />
+            <ProfileCard
+                    v-for="profile in filteredData"
+                    :key="profile.id"
+                    :profile="profile"
+                    class="profile"
+            />
 
-      <div class="icons-note">
-        Icons made by
-        <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from
-        <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
-      </div>
+            <div class="icons-note">
+                Icons made by
+                <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from
+                <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
 import ProfileCard from "./components/ProfileCard";
 
 export default {
-  name: "App",
+    name: "App",
 
-  components: {
-    ProfileCard
-  },
-
-  data() {
-    return {
-      profiles: [
-        {
-          id: 1,
-          name: "Wojciech",
-          email: "wojciech@poz.pl",
-          description: "Anaesthesiologist",
-          likes: 34
-        },
-        {
-          id: 2,
-          name: "Maria",
-          email: "maria@poz.pl",
-          description: "Radiologist",
-          likes: 28
-        },
-        {
-          id: 3,
-          name: "Anna",
-          email: "anna@poz.pl",
-          description: "Surgeon",
-          likes: 53
-        }
-      ]
-    };
-  },
-
-  methods: {
-    sortAsc() {
-      this.profiles.sort(function(a, b) {
-        return a.likes - b.likes;
-      });
+    components: {
+        ProfileCard
     },
 
-    sortDesc() {
-      this.profiles.sort(function(a, b) {
-        return b.likes - a.likes;
-      });
+    data() {
+        return {
+            profiles: [
+                {
+                    id: 1,
+                    name: "Wojciech",
+                    email: "wojciech@poz.pl",
+                    description: "Anaesthesiologist",
+                    likes: 34
+                },
+                {
+                    id: 2,
+                    name: "Maria",
+                    email: "maria@poz.pl",
+                    description: "Radiologist",
+                    likes: 28
+                },
+                {
+                    id: 3,
+                    name: "Anna",
+                    email: "anna@poz.pl",
+                    description: "Surgeon",
+                    likes: 53
+                }
+            ],
+            searchedName: ''
+        };
+    },
+    computed: {
+        filteredData() {
+            if (!this.searchedName) {
+                return this.profiles
+            }
+            return this.profiles.filter(item => {
+                return item.name.toLowerCase().indexOf(this.searchedName.toLowerCase()) !== -1
+            })
+        }
+    },
+    methods: {
+        sortAsc() {
+            this.profiles.sort(function (a, b) {
+                return a.likes - b.likes;
+            });
+        },
+
+        sortDesc() {
+            this.profiles.sort(function (a, b) {
+                return b.likes - a.likes;
+            });
+        }
     }
-  }
 };
 </script>
 
 <style>
 #app {
-  font-family: "Roboto", helvetica, arial, sans-serif;
-  text-align: center;
-  color: #2c3e50;
-  padding: 60px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-  position: relative;
+    font-family: "Roboto", helvetica, arial, sans-serif;
+    text-align: center;
+    color: #2c3e50;
+    padding: 60px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    position: relative;
 
-  background: linear-gradient(
-    135deg,
-    rgba(65, 184, 131, 0.9),
-    rgba(52, 73, 94, 0.9)
-  );
+    background: linear-gradient(
+            135deg,
+            rgba(65, 184, 131, 0.9),
+            rgba(52, 73, 94, 0.9)
+    );
 
-  font-size: 1.5em;
+    font-size: 1.5em;
 }
 
 button {
-  display: block;
-  padding: 1em;
-  width: 100%;
-  background-color: #41B883;
-  border: 1px solid;
-  color: #fff;
-  cursor: pointer;
-  font-size: 0.75em;
-  font-weight: 600;
-  text-shadow: 0 1px 0 rgba(black, 0.2);
+    display: block;
+    padding: 1em;
+    width: 100%;
+    background-color: #41B883;
+    border: 1px solid;
+    color: #fff;
+    cursor: pointer;
+    font-size: 0.75em;
+    font-weight: 600;
+    text-shadow: 0 1px 0 rgba(black, 0.2);
 }
 
 .content {
-  width: 100%;
-  min-width: 300px;
-  padding: 2em;
-  margin-top: 30px;
-  position: relative;
-  background: rgba(0, 0, 0, 0.15);
+    width: 100%;
+    min-width: 300px;
+    padding: 2em;
+    margin-top: 30px;
+    position: relative;
+    background: rgba(0, 0, 0, 0.15);
 }
 
 @media screen and (min-width: 600px) {
-  .content {
-    width: 50vw;
-    max-width: 15em;
-  }
+    .content {
+        width: 50vw;
+        max-width: 15em;
+    }
 }
 
 .content::before {
-  content: "";
-  position: absolute;
-  top: -2px;
-  left: 0;
-  height: 2px;
-  width: 100%;
-  background: #35c3c1;
+    content: "";
+    position: absolute;
+    top: -2px;
+    left: 0;
+    height: 2px;
+    width: 100%;
+    background: #35c3c1;
 }
 
 .buttons {
-  display: flex;
-  box-shadow: 0 10px 10px rgba(0, 0, 0, 0.25), 0 5px 5px rgba(0, 0, 0, 0.22);
+    display: flex;
+    box-shadow: 0 10px 10px rgba(0, 0, 0, 0.25), 0 5px 5px rgba(0, 0, 0, 0.22);
 }
 
 .profile {
-  margin-top: 20px;
+    margin-top: 20px;
 }
 
 .icons-note {
-  margin-top: 30px;
-  font-size: 10px;
+    margin-top: 30px;
+    font-size: 10px;
+}
+.search {
+    margin-bottom: 1rem;
+    width: 100%;
 }
 </style>
