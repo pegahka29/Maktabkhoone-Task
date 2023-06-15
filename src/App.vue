1caa3916751b17b3531f0adb3756132e3579ab2a
pegahka29 <pegik327@gmail.com>
2023-06-15 18:41:48 +0330

    Add new profiles option to add new profiles.

--- src/App.vue
+++ src/App.vue
@@ -2,36 +2,66 @@
     <div id="app">
         <div class="header">Profiles List</div>
         <div class="content">
-            <input  v-model="searchedName" class="search" placeholder="Search by DR Name">
+            <input v-model="searchedName" class="search" placeholder="Search By Doctor Name">
             <div class="buttons">
                 <button @click="sortAsc">▲</button>
                 <button @click="sortDesc">▼</button>
             </div>
-
             <ProfileCard
                     v-for="profile in filteredData"
                     :key="profile.id"
                     :profile="profile"
                     class="profile"
             />
-
             <div class="icons-note">
                 Icons made by
                 <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from
                 <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
             </div>
+            <button @click.prevent="showModal" class="show-modal__button">Add new profile</button>
+            <Modal :show="showAddProfileModal" @close="hideAddProfileModal">
+                <template v-slot:header>
+                    <h3> Add Profile </h3>
+                </template>
+                <template v-slot:body>
+                    <div class="modal-body-input">
+                        <span>Name:</span>
+                        <input v-model="newDoctor.name">
+                    </div>
+                    <div class="modal-body-input">
+                        <span>Email:</span>
+                        <input v-model="newDoctor.email" class="modal-body-input" type="email">
+                    </div>
+                    <div class="modal-body-input">
+                        <span>Description:</span>
+                        <input v-model="newDoctor.description" class="modal-body-input">
+                    </div>
+                </template>
+                <template v-slot:footer>
+                    <button
+                            :disabled="!newDoctor && !newDoctor.email && !newDoctor.description && !newDoctor.name"
+                            :class="newDoctor && newDoctor.email && newDoctor.description && newDoctor.name ? '':'disabled'"
+                            class="modal-add-button"
+                            @click="addNewDoctorProfile"
+                    >
+                        Add
+                    </button>
+                </template>
+            </Modal>
         </div>
     </div>
 </template>
 
 <script>
-import ProfileCard from "./components/ProfileCard";
+import ProfileCard from "./components/ProfileCard"
+import Modal from "./components/Modal"
 
 export default {
     name: "App",
 
     components: {
-        ProfileCard
+        ProfileCard,
+        Modal
     },
 
     data() {
@@ -59,7 +89,14 @@ export default {
                     likes: 53
                 }
             ],
-            searchedName: ''
+            searchedName: "",
+            showAddProfileModal: false,
+            newDoctor: {
+                name: null,
+                email: null,
+                description: null,
+                likes: 0
+            }
         };
     },
     computed: {
@@ -83,14 +120,43 @@ export default {
             this.profiles.sort(function (a, b) {
                 return b.likes - a.likes;
             });
+        },
+
+        showModal() {
+            this.showAddProfileModal = true
+        },
+        hideAddProfileModal() {
+            this.showAddProfileModal = false
+        },
+        addNewDoctorProfile() {
+            this.profiles.push({
+                id: this.profiles.length + 1,
+                name: this.newDoctor.name,
+                likes: this.newDoctor.likes,
+                description: this.newDoctor.description,
+                email: this.newDoctor.email
+            })
+            this.newDoctor.name = ''
+            this.newDoctor.description = ''
+            this.newDoctor.email = ''
+            this.showAddProfileModal = false
         }
     }
 };
 </script>
 
 <style>
+
+html, body {
+    padding: 0;
+    margin: 0;
+    width: 100vw;
+}
+
 #app {
     font-family: "Roboto", helvetica, arial, sans-serif;
+    width: 100%;
+    height: 100%;
     text-align: center;
     color: #2c3e50;
     padding: 60px;
@@ -161,8 +227,34 @@ button {
     margin-top: 30px;
     font-size: 10px;
 }
+
 .search {
     margin-bottom: 1rem;
     width: 100%;
+}
+
+.modal-body-input {
+    margin-top: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+}
+
+.modal-add-button {
+    height: 48px;
+    width: 30%;
+    min-width: 280px;
+    max-width: 300px;
+    padding: 0 3px;
+    box-sizing: content-box;
+    margin-top: 1rem;
+}
+
+.show-modal__button {
+    margin-top: 1rem;
+}
+.disabled {
+    background: darkgray !important;
 }
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,120 @@
+<template>
+    <Transition name="fade">
+            <div v-if="show" class="modal-mask">
+                <div class="modal-wrapper">
+                    <div class="modal-container">
+                        <button
+                            class="modal-close-button"
+                            @click="$emit('close')"
+                        >❌
+                        </button>
+                        <div class="modal-header">
+                            <slot name="header">default header</slot>
+                        </div>
+
+                        <div class="modal-body">
+                            <slot name="body">default body</slot>
+                        </div>
+
+                        <div class="modal-footer">
+                            <slot name="footer">
+                                default footer
+                            </slot>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </Transition>
+</template>
+
+<script>
+
+export default ({
+    name: "Modal",
+    props: {
+        show: Boolean
+    }
+})
+</script>
+
+<style>
+.modal-mask {
+    position: fixed;
+    z-index: 1000;
+    top: 0;
+    right: 0;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    -webkit-backdrop-filter: blur(5px);
+    backdrop-filter: blur(5px);
+    display: table;
+    -webkit-transition: opacity 0.3s ease;
+    transition: opacity 0.3s ease;
+}
+
+.modal-wrapper {
+    display: table-cell;
+    vertical-align: middle;
+}
+
+.modal-container {
+    width: 70%;
+    min-width: 300px;
+    min-height: 300px;
+    max-width: 600px;
+    margin: 0 auto;
+    background-color: #fff;
+    border-radius: 2px;
+    -webkit-box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+    -webkit-transition: all 0.3s ease;
+    transition: all 0.3s ease;
+    padding-bottom: 30px;
+}
+
+.modal-header h3 {
+    margin-top: 0;
+    color: #42b983;
+}
+
+.modal-body {
+    margin: 20px 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+.modal-body input {
+    height: 48px;
+    width: 30%;
+    min-width: 280px;
+    max-width: 300px;
+}
+.modal-body button {
+    height: 48px;
+    width: 50%;
+    min-width: 100px;
+    max-width: 200px;
+}
+.modal-close-button {
+    background: transparent !important;
+    display: flex;
+    justify-content: flex-end;
+}
+.modal-footer {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.fade-enter-active, .fade-leave-active {
+    transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+    opacity: 0;
+}
+
+</style>
